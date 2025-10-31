006859216688b6e413902f67b64613e698d862cf
instantuzer <152444996+instantuzer@users.noreply.github.com>
2025-10-31 13:22:55 +0000

    Fix installation script syntax and improve prompts

--- install.sh
+++ install.sh
@@ -17,13 +17,11 @@ while true; do
     fi
 done
 
-read -rp "Enter hostname for the system: " HOSTNAME
-
 # --- Disk selection (default to /dev/sda) ---
 DISK="/dev/nvme0n1"
 echo "Installing to $DISK (all data on this disk will be erased!)"
-read -rp "Are you sure? (yes/no): " CONFIRM
-if [ "$CONFIRM" != "yes" ]; then
+read -rp "Are you sure? (y/n): " CONFIRM
+if [ "$CONFIRM" != "y" ]; then
     echo "Installation aborted."
     exit 1
 fi
@@ -35,15 +33,15 @@ sgdisk -n 1::+3G -t 1:EF00 -c 1:"EFI" $DISK
 sgdisk -n 2::+275G -t 2:8300 -c 2:"ROOT" $DISK
 
 # --- Formatting ---
-mkfs.fat ${DISK}1
-mkfs.ext4 ${DISK}2
+mkfs.fat ${DISK}p1
+mkfs.ext4 ${DISK}p2
 
 # --- Mounting ---
-mount ${DISK}2 /mnt
-mount -m ${DISK}1 /mnt/boot
+mount ${DISK}p2 /mnt
+mount -m ${DISK}p1 /mnt/boot
 
 # --- Install base system ---
-pacstrap /mnt base linux-zen linux-firmware-intel intel-ucode vim nvim sudo git networkmanager grub efibootmgr xorg xorg-xinit xf86-video-intel libva-intel-driver i3 alacritty rofi firefox noto-fonts exa jq bc zip unzip lolcat xcolor pipewire pipewire-pulse brigthnessctl playerctl mtpfs
+pacstrap /mnt base linux-zen linux-firmware-intel intel-ucode vim nvim sudo git networkmanager grub efibootmgr xorg xorg-xinit xf86-video-intel libva-intel-driver i3 alacritty rofi firefox noto-fonts exa jq bc zip unzip lolcat xcolor pipewire pipewire-pulse brightnessctl playerctl mtpfs
 
 # --- Fstab ---
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -53,7 +51,7 @@ arch-chroot /mnt /bin/bash <<EOF
 set -e
 
 # Hostname
-echo "$HOSTNAME" > /etc/hostname
+echo "staticlol" > /etc/hostname
 
 # Timezone and localization
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
@@ -66,12 +64,15 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "root:$PASSWORD" | chpasswd
 
 # User creation
-useradd -m -G wheel -s /bin/bash $USERNAME
+useradd -mG wheel -s /bin/bash $USERNAME
 echo "$USERNAME:$PASSWORD" | chpasswd
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
 grub-install --efi-directory=/boot
 grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+
 EOF
 
 echo "Installation complete! Reboot now."
